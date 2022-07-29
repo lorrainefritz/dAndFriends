fd73fe98776806983217b6f7455774c330ccf0e6
lorrainefritz <lorraine.fritz0@gmail.com>
2022-07-29 14:42:23 +0200

    firstCommit

---
+++ src/main/java/com/oc/dandfriends/DAndFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.oc.dandfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DAndFriendsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DAndFriendsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oc/dandfriends/DAndFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc.dandfriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DAndFriendsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
